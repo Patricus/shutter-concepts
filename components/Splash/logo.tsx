ce85b285aa7c746ca2713b0c3a300cc1e4394d91
Patrick McPherson <patrick.j.mcpherson@gmail.com>
2023-02-23 23:13:19 -0800

    Optimize logo

--- components/Splash/logo.tsx
+++ components/Splash/logo.tsx
@@ -1,20 +1,23 @@
 import React from "react";
 import Image from "next/image";
 import styles from "@/styles/Logo.module.css";
+import logo from "@/public/images/logo_transparent.webp";
 
-function logo() {
+function Logo() {
     return (
         <div className={styles.container}>
             <div className={styles.logoContainer}>
                 <Image
                     //blurDataUrl is a base64 encoded image that is used as a placeholder while the image is loading, here we use code to generate a semi-transparent gray image
                     blurDataURL="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mOMvgwAAY8BMGsecTMAAAAASUVORK5CYII="
-                    src="images/logo_transparent.png"
+                    src={logo}
                     alt="Shutter Concepts Logo"
-                    className={styles.logo}
-                    width={150}
-                    height={150}
-                    quality={100}
+                    sizes="150px"
+                    // className={styles.logo}
+                    fill
+                    // width={150}
+                    // height={150}
+                    // quality={100}
                     placeholder="blur"
                 />
             </div>
@@ -22,4 +25,4 @@ function logo() {
     );
 }
 
-export default logo;
+export default Logo;
